98784c245c82fc9cb8570781ecd514d54d20e40c
davronbekmuysinov <davronbekmuysinov@gmail.com>
2026-06-07 13:37:08 +0500

    updated

--- script.js
+++ script.js
@@ -105,7 +105,7 @@
 for (let i = 0; i <= 10; i++) {
 console.log(i*2);}
 
-// const alls = ["Red", "blue", "yellow", "black", "white"]
-// for(let numbers of alls ){
-//     console.log(numbers.toUpperCase());
-//}
+const alls = ["Red", "blue", "yellow", "black", "white"]
+for(let numbers of alls ){
+    console.log(numbers.toUpperCase());
+}
