b421cb26c54c2746b85b3130e1fce63eec49556f
Muysinov Davronbek <davronbekmuysinov@gmail.com>
2026-06-07 12:20:57 +0500

    added js codes

---
+++ script.js
@@ -0,0 +1,111 @@
+// console.log(2+2)
+
+
+// alert("hello word")
+
+
+// const ism = prompt("Ismingiz:")
+// console.log(ism);
+// alert(ism)
+
+// 1-vazifa
+// const yosh = prompt("Yoshim:")
+
+// const ism = prompt("Ismim:")
+
+// const shahar = prompt("Shahar:")
+
+// const noutbuk = prompt("Noutbukim brendi:")
+
+// console.log("Yoshim: " + yosh + " Ismim: " + ism +" Shahar: " + shahar + " Noutbukim brendi: " + noutbuk)
+
+
+// 2-vazifa
+// let x = 20
+// let y = 4
+
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(x/y)
+
+
+// 3-vazifa
+// const maktab = 'Iqtidor';
+
+
+// maktab = 'Zamonaviy'
+
+
+// let person = {
+//     ism: "Davron", 
+//     manzil: "Kitob", 
+//     phone: 880122015, 
+//     email: "davronbekmuysinov@gmail.com"
+// }
+
+// let car = {
+//     ism: "Damas", 
+//     manzil: "Kitob", 
+//     phone: "O001OO", 
+//     tezlik: 220
+// }
+
+
+// let phone = {
+//     ism: "Samsung", 
+//     manzil: "Kitob", 
+//     phone: 880122015, 
+//     ram: "8GB"
+// }
+
+
+// let things = [person, phone, car]
+
+// console.log(things)
+
+
+// trim
+// let text = "     Davron      "
+// console.log(text.trim())
+
+// replace
+// let user = "Davron"
+// console.log(user.replace("D", "S"))
+
+// repeat
+// let text = "Davron"
+// console.log(text.repeat(2))
+
+// split
+// let text = "Davron, Davron"
+// console.log(text.split(","))
+
+//  let ism = prompt("Ism:")
+//  console.log(ism.trim().slice(-0, 1).toUpperCase);
+
+
+// var yosh ="18"
+// var butunson = parseInt(yosh)
+// console.log(typeof butunson)
+
+// let maosh = "500";
+// let natija = maosh * 2;
+// console.log(typeof natija)
+
+// let id = 9876;
+// let matn = id.toString(); 
+// String()
+// console.log( matn)
+
+// let balans = 0;
+// let matn = Boolean(balans);
+// console.log( matn)
+
+// for (let i = 0; i <= 10; i++) {
+// console.log(i*2);}
+
+// const alls = ["Red", "blue", "yellow", "black", "white"]
+// for(let numbers of alls ){
+//     console.log(numbers.toUpperCase());
+//}
